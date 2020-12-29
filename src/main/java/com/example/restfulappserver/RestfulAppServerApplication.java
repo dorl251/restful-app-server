709cf3b8cdd0893f7fdc5edb554cffc3b11303a4
jeonghoon <tghyyhju@gmail.com>
2020-12-30 06:17:59 +0900

    update

---
+++ src/main/java/com/example/restfulappserver/RestfulAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulAppServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulAppServerApplication.class, args);
+    }
+
+}
