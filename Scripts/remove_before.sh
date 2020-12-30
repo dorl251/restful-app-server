17dce63ec0e6b579781813fd72a9586e912d5e53
jeonghoon <tghyyhju@gmail.com>
2020-12-30 12:47:41 +0900

    update

--- Scripts/remove_before.sh
+++ Scripts/remove_before.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-find /opt/codedeploy-agent/deployment-root/AppServer/* -maxdepth 0 -type 'd' | grep -v
-$(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/AppServer/* | sort -t: -n | tail -1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
+find /opt/codedeploy-agent/deployment-root/68a9eade-af15-48ac-92a9-35d36d453e73/* -maxdepth 0 -type 'd' | grep -v
+$(stat -c '%Y:%n' /opt/codedeploy-agent/deployment-root/68a9eade-af15-48ac-92a9-35d36d453e73/* | sort -t: -n | tail -1 | cut -d: -f2- | cut -c 3-) | xargs rm -rf
 sudo rm -rf /home/ec2-user/*
